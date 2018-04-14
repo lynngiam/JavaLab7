d87685ff165b8ef6585e62e8791b77a03b243c20
lynngiam <37986479+lynngiam@users.noreply.github.com>
2018-04-13 22:49:11 -0400

    Add files via upload

--- RBTreeMapTest.java
+++ RBTreeMapTest.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.Iterator;
 import java.util.Map;
@@ -34,40 +33,55 @@ public class RBTreeMapTest {
 	while (it.hasNext()) {
 	    System.out.print(it.next() + " ");
 	}
+	System.out.println();
 	assertTrue("Confirm properties: ", test.verify());
 
     }
 
     @Test
-    public void testLL() {
-	// single right rotation
+    public void testDoubleRotations() {
 	RBTreeMap<Integer, String> test = new RBTreeMap<Integer, String>();
 	test.put(25, "A");
 	test.put(5, "F");
 	test.put(10, "B");
+	// LR; rotate left then right
 	test.put(9, "C");
+	// recolors
 	test.put(6, "D");
+	// RL; rotate right then left
 	test.put(4, "E");
 	Iterator<Map.Entry<Integer, String>> it = test.entries();
 	while (it.hasNext()) {
 	    System.out.print(it.next() + " ");
 	}
+	System.out.println();
 	assertTrue("Confirm properties: ", test.verify());
     }
 
     @Test
-    public void testRL() {
-	// single left rotation then
+    public void testSingleRotation() {
+	RBTreeMap<Integer, String> test = new RBTreeMap<Integer, String>();
+	test.put(25, "A");
+	test.put(5, "F");
+	test.put(4, "B");
+	// rotate right
+	test.put(60, "C");
+	test.put(70, "Y");
+	// rotate left
+	assertTrue("Confirm properties: ", test.verify());
     }
 
     @Test
-    public void testRBTreeMapKVRBTreeMapOfKVRBTreeMapOfKV() {
-	fail("Not yet implemented");
-    }
-
-    @Test
-    public void testRBTreeMapKV() {
-	fail("Not yet implemented");
+    public void testExistingValue() {
+	RBTreeMap<Integer, String> test = new RBTreeMap<Integer, String>();
+	test.put(0, "A");
+	test.put(1, "B");
+	test.put(2, "C");
+	test.put(3, "D");
+	test.put(4, "E");
+	test.put(3, "R");
+	assertEquals("Get value at key: ", "R", test.get(3));
+	assertTrue("Confirm properties: ", test.verify());
     }
 
 }
